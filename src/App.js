20d68607fab61a1f7727154678aef9eb3b4f4624
huguangze <2581504687@qq.com>
2024-02-20 15:45:27 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {HashRouter  } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import 'antd/dist/reset.css';
 import Router from "./routes"
 // import AuthRouter from './routes/utils/AuthRouter';
 export default function App() {
-  
+
   return (
-    <div id='main' style={{width:'100%',height:"100%"}}>
-         <HashRouter >
-          {/* <AuthRouter> */}
-          <Router />
-          {/* </AuthRouter> */}
-      </HashRouter >
+    <div id='main' style={{ width: '100%', height: "100%" }}>
+      <BrowserRouter >
+        {/* <AuthRouter> */}
+        <Router />
+        {/* </AuthRouter> */}
+      </BrowserRouter >
     </div>
   )
 }
--- src/routes/modules/Layout.jsx
+++ src/routes/modules/Layout.jsx
@@ -1,68 +1,70 @@
-import React, { Suspense } from "react"
-import { Navigate } from "react-router-dom"
-import About from "@/pages/About"
-import Layout from "@/pages/Layout"
-import LazyLoad from "../utils/LazyLoad"
+import React, { Suspense } from 'react'
+import { Navigate } from 'react-router-dom'
+import About from '@/pages/About'
+import Layout from '@/pages/Layout'
+import LazyLoad from '../utils/LazyLoad'
 // https://hanlab.mit.edu/
 export const UserLayoutRouter = [
   {
     element: <Layout />,
     children: [
       {
-        path: "/",
-        element: <Navigate to="/home" />,
+        path: '/',
+        element: <Navigate to="/team" />,
       },
       {
-        path: "/home",
+        path: '/team',
         element: <About />,
       },
       {
-        path: "/achievementDetail",
-        element: LazyLoad(React.lazy(() => import("@/pages/AchievementDetail"))),
+        path: '/achievementDetail',
+        element: LazyLoad(
+          React.lazy(() => import('@/pages/AchievementDetail'))
+        ),
       },
       {
-        path: "/news",
-        element: LazyLoad(React.lazy(() => import("@/pages/News"))),
+        path: '/news',
+        element: LazyLoad(React.lazy(() => import('@/pages/News'))),
       },
       {
-        path: "/awards",
-        element: LazyLoad(React.lazy(() => import("@/pages/Awards"))),
+        path: '/awards',
+        element: LazyLoad(React.lazy(() => import('@/pages/Awards'))),
       },
       {
-        path: "/blog",
-        element: LazyLoad(React.lazy(() => import("@/pages/Blog"))),
+        path: '/blog',
+        element: LazyLoad(React.lazy(() => import('@/pages/Blog'))),
       },
       {
-        path: "/course",
-        element: LazyLoad(React.lazy(() => import("@/pages/Course"))),
+        path: '/course',
+        element: LazyLoad(React.lazy(() => import('@/pages/Course'))),
       },
       {
-        path: "/courseDetail",
-        element: LazyLoad(React.lazy(() => import("@/pages/CourseDetail"))),
+        path: '/courseDetail',
+        element: LazyLoad(React.lazy(() => import('@/pages/CourseDetail'))),
       },
       {
-        path: "/gallery",
-        element: LazyLoad(React.lazy(() => import("@/pages/Gallery"))),
+        path: '/gallery',
+        element: LazyLoad(React.lazy(() => import('@/pages/Gallery'))),
       },
       {
-        path: "/media",
-        element: LazyLoad(React.lazy(() => import("@/pages/Media"))),
+        path: '/media',
+        element: LazyLoad(React.lazy(() => import('@/pages/Media'))),
       },
       {
-        path: "/publications",
-        element: LazyLoad(React.lazy(() => import("@/pages/Publications"))),
+        path: '/publications',
+        element: LazyLoad(React.lazy(() => import('@/pages/Publications'))),
       },
       {
-        path: "/talks",
-        element: LazyLoad(React.lazy(() => import("@/pages/Talks"))),
+        path: '/talks',
+        element: LazyLoad(React.lazy(() => import('@/pages/Talks'))),
       },
       {
-        path: "/team",
-        element: LazyLoad(React.lazy(() => import("@/pages/Team"))),
+        path: '/team',
+        element: LazyLoad(React.lazy(() => import('@/pages/Team'))),
       },
       {
-        path: "/memberDetail",
-        element: LazyLoad(React.lazy(() => import("@/pages/MemberDetail"))),
+        path: '/memberDetail',
+        element: LazyLoad(React.lazy(() => import('@/pages/MemberDetail'))),
       },
     ],
   },
